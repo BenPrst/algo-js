4ea0e8b29cdf4becbadcbfd56192841440a1e751
Benjamin Porsont <porsont.benjamin@gmail.com>
2022-11-27 01:47:54 +0100

    modif

---
+++ JS1.0/javascript.js
@@ -0,0 +1,8 @@
+let age = 21;
+console.log(age);
+
+let name = " Porsont";
+let firstName = "Benjamin";
+let city = "Charleroi";
+
+console.log("My name is " + firstName + name + " and i live in " + city);
---
+++ JS2.0/JS2.1/javascript.js
@@ -0,0 +1,6 @@
+let age = 18;
+const a = Number(prompt("How old are you?", ""));
+
+if (a < 18) {
+  console.log("You are not yet an adult");
+} else console.log("You are an adult");
